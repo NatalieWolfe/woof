1106348a841934bcac43e9fc091c14d504c1d2d9
Natalie Wolfe <natalie@lifewanted.com>
2022-08-06 20:56:27 -0700

    Copy StringBuffer from liblw.

---
+++ woof/io/stream/buffer_test.cpp
@@ -0,0 +1,31 @@
+#include "woof/io/stream/buffer.h"
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+namespace woof::io::stream {
+namespace {
+
+TEST(StringBufferTest, WorksWithOStream) {
+  StringBuffer buffer;
+  std::ostream o{&buffer};
+  o << "Hello, World!";
+  EXPECT_EQ(buffer.string(), "Hello, World!");
+}
+
+TEST(StringBufferTest, WorksWithIStream) {
+  StringBuffer buffer;
+  {
+    std::ostream o{&buffer};
+    o << "Hello, World!";
+  }
+
+  std::istream i{&buffer};
+  std::string word;
+  i >> word;
+  EXPECT_EQ(word, "Hello,");
+}
+
+}
+}
